fe706c0e11075c8a06b5bc3b75ce97993025e41d
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-10-15 05:55:32 +0200

    Add index.js - function: showUpdatedScores(); eventListeners: fomr - on submit, refresh button - on click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
 import './style.css';
-import { scoreListExample, generateScoreListHtml } from './utils.js';
+import { readScores, postNewScore } from './leaderboard_api.js';
+import { generateScoreListHtml, getRankedScoreList } from './utils.js';
 
-const listUL = document.querySelector('[data-score-list]');
+const ulContainer = document.querySelector('[data-score-list]');
+const refresh = document.querySelector('[data-refresh]');
+const form = document.querySelector('[data-form-add]');
+const nameInputField = document.getElementById('name');
+const scoreInputField = document.getElementById('score');
 
-generateScoreListHtml(scoreListExample, listUL);
+const showUpdatedScores = async () => {
+  const scoresFromApi = await readScores();
+  const list = getRankedScoreList(scoresFromApi.result);
+  generateScoreListHtml(list, ulContainer);
+};
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const user = nameInputField.value;
+  const score = scoreInputField.value;
+  await postNewScore({ user, score });
+  nameInputField.value = '';
+  scoreInputField.value = '';
+  await showUpdatedScores();
+});
+
+refresh.addEventListener('click', async () => {
+  await showUpdatedScores();
+});
+
+window.onload = async () => {
+  await showUpdatedScores();
+};
